45d930f967a9aaf437aca44179e7ac822d595db5
Djamelikra <91476697+Djamelikra@users.noreply.github.com>
2022-08-01 23:08:32 +0200

    compare tab

--- main.c
+++ main.c
@@ -6,13 +6,37 @@ int main()
 {
     char str1[100];
     char str2[100];
+    int i;
     printf("Enter the first string\n");
     gets(str1);
     printf("Enter the second string\n");
     gets(str2);
 
+    for(i=0; i<5; i++)
+    {
+        printf("%c",str1[i]);
+
+    }
+    printf("\n");
+
     if(strcmp(str1,str2)== 0)
-        printf("The two strings are equals.\n");
+    {
+
+
+    printf("The two strings : \n");
+       for(i=0; i<5; i++)
+    {
+        printf("%c",str1[i]);
+
+    }
+     printf("\n");
+          for(i=0; i<5; i++)
+    {
+        printf("%c",str2[i]);
+
+    }
+    printf("...are equals.\n");
+     }
     else
         printf("The two strings are not equals.\n");
 
