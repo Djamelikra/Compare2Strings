58f6a1d92b53307932d8768baae661de6872872f
Djamelikra <91476697+Djamelikra@users.noreply.github.com>
2022-08-02 19:44:44 +0200

    sizeof

--- main.c
+++ main.c
@@ -7,12 +7,23 @@ int main()
     char str1[100];
     char str2[100];
     int i;
+
+    char kar = 'a';
+    int taille = sizeof(kar);
+    printf("taille en octets : %u \n",taille);
+
     printf("Enter the first string\n");
     gets(str1);
+    taille = sizeof(str1)/sizeof str1[0];
+
+    //str1[taille];
+    printf("Longueur du tableau : %d \n",taille);
+
+
     printf("Enter the second string\n");
     gets(str2);
 
-    for(i=0; i<5; i++)
+    for(i=0; i<65; i++)
     {
         printf("%c",str1[i]);
 
@@ -24,21 +35,25 @@ int main()
 
 
     printf("The two strings : \n");
-       for(i=0; i<5; i++)
+       for(i=0; i<65; i++)
     {
         printf("%c",str1[i]);
 
     }
      printf("\n");
-          for(i=0; i<5; i++)
+          for(i=0; i<65; i++)
     {
         printf("%c",str2[i]);
 
     }
-    printf("...are equals.\n");
+    printf("\n...are equals.\n");
      }
     else
         printf("The two strings are not equals.\n");
+
+/*taille */
+    taille = sizeof(str1);
+      printf("taille en octets : %u \n",taille);
 
 //    printf("Press any key to continue\n");
 //    getchar();
